44fdf418f286d2625e8420a5da416903553908af
Michael Bedward <michael.bedward@gmail.com>
2021-12-03 15:26:55 +1100

    Added ldb_wake_up

---
+++ R/utility_functions.R
@@ -0,0 +1,43 @@
+#' Wake up a possibly dormant database connection
+#'
+#' When working with very large LAS files, loading and processing a file can
+#' take so long that the connection to a database server goes to sleep. This
+#' function submits a trivial query to the server, repeating if necessary
+#' every \code{retry_seconds}, to wake the connection up. Note that this
+#' function will not re-open a connection that has been closed.
+#'
+#' @param db An open but possibly sleeping database connection.
+#'
+#' @param retry_seconds Number of seconds in between repeated attempts to wake
+#'   the connection.
+#'
+#' @param max_tries Maximum number of times to try waking the connection. The
+#'   default is 5. Set to Inf for no limit.
+#'
+#' @examples
+#' \dontrun {
+#' # After taking some minutes to read a very large LiDAR file...
+#' ldb_wake_up(DB)
+#'
+#' # ...now the connection is awake and ready to use again
+#' ldb_load_tile_metadata(db, las, filename = "foo.las")
+#' }
+#'
+#' @export
+#'
+ldb_wake_up <- function(db, retry_seconds = 3, max_tries = 5) {
+  x <- NULL
+
+  itry <- 1
+  while (is.null(x) && itry <= max_tries) {
+    x <- tryCatch(
+      DBI::dbGetQuery(db, "select 1;"),
+      error = function(e) NULL)
+
+    if (is.null(x)) {
+      itry <- itry + 1
+      Sys.sleep(retry_seconds)
+    }
+  }
+}
+
