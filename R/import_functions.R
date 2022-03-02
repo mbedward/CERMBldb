0af485ce0ccc5d398a53485a0d3a73dcefe02e39
Michael Bedward <michael.bedward@gmail.com>
2022-03-02 17:51:45 +1100

    ldb_load_vegheight_raster()

--- R/import_functions.R
+++ R/import_functions.R
@@ -53,7 +53,7 @@
 #'
 #' @param crs_fallback The coordinate reference system to apply if one cannot be
 #'   retrieved from the input LAS object. This can either be an object of class
-#'   \code{'crs'} created with function \code{\link[sf]{st_crs}, an integer EPSG
+#'   \code{'crs'} created with function \code{\link[sf]{st_crs}}, an integer EPSG
 #'   code, or any spatial object from which a CRS can be extracted. The default
 #'   value \code{NULL}, or a missing value will cause the function to stop with
 #'   an error if the LAS object does not have a CRS.
@@ -119,9 +119,9 @@ ldb_load_tile_metadata <- function(db,
                        select 1 from supported_crs
                        where srid = {epsgcode} );")
 
-  res <- dbGetQuery(db, cmd)
+  res <- DBI::dbGetQuery(db, cmd)
   if (!res$exists[1]) {
-    msg <- glue::glue("The LiDAR tile coordinate reference system (EPSG:{epsgcode})
+    msg <- glue::glue("The coordinate reference system (EPSG:{epsgcode})
                        is not (yet) supported by the database.")
     stop(msg)
   }
@@ -147,7 +147,7 @@ ldb_load_tile_metadata <- function(db,
 
   # Reproject bounds into the CRS used by the metadata table
   cmd <- "select Find_SRID('lidar', 'metadata', 'geom') AS srid;"
-  res <- dbGetQuery(db, cmd)
+  res <- DBI::dbGetQuery(db, cmd)
 
   if (nrow(res) != 1) {
     msg <- glue::glue("Something really bad has happened!!!
@@ -220,12 +220,10 @@ ldb_load_tile_metadata <- function(db,
 #'
 #' This function associates a multi-band raster of point counts, derived from a
 #' LAS point cloud using the \code{CERMBlidar::get_stratum_counts} function,
-#' with an existing meta-data record for the LAS tile. Usually, it will be
-#' called from the \code{ldb_import_tile()} function, but can also be called
-#' directly if the integer ID value of the associated meta-data table record is
-#' known. The database uses \emph{out-db} storage for raster files, where a
-#' raster table record stores the path to the file location, which will usually
-#' be AWS S3-compatible storage although other types of storage can be used.
+#' with an existing meta-data record for the LAS tile. The database uses
+#' \emph{out-db} storage for raster files, where a raster table record stores
+#' the path to the file location, which will usually be AWS S3-compatible
+#' storage although other types of storage can be used.
 #'
 #' If any credentials are required to access the file (e.g. AWS Access Key) it
 #' is assumed that this have been set in the environment from which the function
@@ -284,8 +282,6 @@ ldb_load_tile_metadata <- function(db,
 #'
 #' @export
 #'
-# Function to load one or more rasters given the S3 URL for the GeoTIFF file(s)
-#
 ldb_load_pointcount_raster <- function(db,
                                        raster_url,
                                        strata_def = "cermb",
@@ -326,7 +322,7 @@ ldb_load_pointcount_raster <- function(db,
   # Check that the number of layers corresponds to the
   # strata definition
   cmd <- glue::glue("select * from stratum where strata_def = '{strata_def}';")
-  dat_strata <- dbGetQuery(db, cmd)
+  dat_strata <- DBI::dbGetQuery(db, cmd)
 
   if (nrow(dat_strata) == 0) {
     msg <- glue::glue("'{strata_def}' is not recognized as a supported
@@ -358,9 +354,9 @@ ldb_load_pointcount_raster <- function(db,
                      from supported_crs
                      where srid = {EPSG};")
 
-  res <- dbGetQuery(db, cmd)
+  res <- DBI::dbGetQuery(db, cmd)
   if (nrow(res) != 1) {
-    msg <- glue::glue("The LiDAR tile coordinate reference system (EPSG:{epsgcode})
+    msg <- glue::glue("The coordinate reference system (EPSG:{epsgcode})
                        is not (yet) supported by the database.")
     stop(msg)
   }
@@ -380,7 +376,7 @@ ldb_load_pointcount_raster <- function(db,
                          where tile_id = {tile_id}
                        );")
 
-    ok <- dbGetQuery(db, cmd)$exists
+    ok <- DBI::dbGetQuery(db, cmd)$exists
 
     if (!ok) {
       msg <- glue::glue("No meta-data record was found with tile_id = {tile_id}.")
@@ -398,7 +394,7 @@ ldb_load_pointcount_raster <- function(db,
                        datum = '{DATUM}' and
                        zone = {ZONE};")
 
-    res <- dbGetQuery(db, cmd)
+    res <- DBI::dbGetQuery(db, cmd)
 
     if (nrow(res) == 0) {
       msg <- glue::glue("No existing record was found that corresponds to this
@@ -422,7 +418,7 @@ ldb_load_pointcount_raster <- function(db,
                       where tile_id = {tile_id}
                     )")
 
-  res <- dbGetQuery(db, cmd)
+  res <- DBI::dbGetQuery(db, cmd)
   if (res$exists) {
     msg <- glue::glue("There is an existing raster record in the raster table
                        pointcounts_{suffix} with a matching tile_id value ({tile_id}).
@@ -445,7 +441,7 @@ ldb_load_pointcount_raster <- function(db,
 
   # Run the generated SQL to load the raster to the temporary load table
   queries <- readLines(fsql)
-  sapply(queries, function(q) dbExecute(db, q))
+  sapply(queries, function(q) DBI::dbExecute(db, q))
 
   # Copy the raster from the temp_load table to the point
   # counts table for this map zone
@@ -456,11 +452,243 @@ ldb_load_pointcount_raster <- function(db,
     {tile_id} as tile_id,
     rast from lidar.{TEMP_LOAD_TABLE};")
 
-  res <- dbExecute(db, cmd)
+  res <- DBI::dbExecute(db, cmd)
 
   # Drop the temp load table
   cmd <- glue::glue("drop table lidar.{TEMP_LOAD_TABLE};")
-  dbExecute(db, cmd)
+  DBI::dbExecute(db, cmd)
+
+  if (!is.null(res) && res > 0) {
+    TRUE
+  } else {
+    FALSE
+  }
+}
+
+
+#' Create a raster table record for vegetation height derived from a LAS tile
+#'
+#' This function associates a single-band raster of vegetation height, usually
+#' derived from a LAS point cloud using the \code{CERMBlidar::get_max_height}
+#' function, with an existing meta-data record for the LAS tile. The database
+#' uses \emph{out-db} storage for raster files, where a raster table record
+#' stores the path to the file location, which will usually be AWS S3-compatible
+#' storage although other types of storage can be used.
+#'
+#' If any credentials are required to access the file (e.g. AWS Access Key) it
+#' is assumed that this have been set in the environment from which the function
+#' is being called.
+#'
+#' @param db An open database connection for a user with administrator rights
+#'   to the database.
+#'
+#' @param raster_url A URL giving the file location, e.g. in an accessible S3
+#'   bucket.
+#'
+#' @param raster_crs The coordinate reference system of the raster file in a
+#'   form that can be understood by the \code{sf::st_crs()} function (e.g. a WKT
+#'   string or an integer EPSG code. This is used to infer the name of the
+#'   relevant meta-data and raster tables in the database. If \code{NULL}
+#'   (default), the function will attempt to retrieve the CRS from the raster
+#'   file.
+#'
+#' @param tile_id Integer ID for the corresponding existing record in the
+#'   'metadata' table. If \code{NULL} (default), the function will search for a
+#'   meta-data record based on the file name of the input raster.
+#'
+#' @param protocol The name of a GDAL-supported virtual file system protocol
+#'   to use when accessing the raster file from storage. The default is
+#'   \code{'vsicurl'}. See
+#'   \href{https://gdal.org/user/virtual_file_systems.html}{GDAL online documentation}
+#'   for other options. This will be stored as part of the raster file path in
+#'   the database raster table record.
+#'
+#' @param tilew The tile size to use for the raster.
+#'   See this \href{https://blog.crunchydata.com/blog/postgis-raster-and-crunchy-bridge}{
+#'   Postgis raster example} for more details.
+#'
+#' @param R2P The path to the Postgis command line program \code{raster2pgsql}.
+#'   If \code{NULL} or not found, the function will attempt to locate the
+#'   program on the client system.
+#'
+#' @param host Host/server on which the database resides. If \code{NULL}
+#'   (default), will be determined from the database connection object.
+#'
+#' @param dbname Database name. If \code{NULL} (default), will be determined
+#'   from the database connection object.
+#'
+#' @param user User name. If \code{NULL} (default), will be determined from the
+#'   database connection object.
+#'
+#' @param password User password. If \code{NULL}, this will automatically fall
+#'   back to the user's \code{pgpass.conf} file, if one exists, or the
+#'   \code{PGPASSWORD} environment variable. On a Windows client, the
+#'   \code{pgpass.conf} file in the directory
+#'   \code{~/AppData/Roaming/postgresql}.
+#'
+#' @export
+#'
+ldb_load_vegheight_raster <- function(db,
+                                      raster_url,
+                                      raster_crs = NULL,
+                                      tile_id = NULL,
+                                      protocol = "vsicurl",
+                                      tilew = 256,
+                                      R2P = "C:/Program Files/PostgreSQL/12/bin/raster2pgsql.exe",
+                                      host = NULL,
+                                      dbname = NULL,
+                                      username = NULL,
+                                      password = NULL) {
+
+  protocol <- stringr::str_trim(protocol[1])
+  if (!grepl("^/", protocol)) protocol <- glue::glue("/{protocol}")
+
+  vsi_url <- glue::glue("{protocol}/{raster_url}")
+
+  # Check that the raster2pgsql program can be found
+  if (is.null(R2P)) {
+    R2P <- .find_raster2pgsql()
+  } else {
+    info <- file.info(R2P)
+
+    if (is.na(info$exe)) {
+      msg <- glue::glue("The raster2psql program was not found at {R2P}.")
+      stop(msg)
+
+    } else if (info$exe == "no") {
+      msg <- glue::glue("The supplied path for R2P ({R2P})
+                         is not an executable file.")
+      stop(msg)
+    }
+  }
+
+  r <- terra::rast(vsi_url)
+  rcrs <- sf::st_crs(r)
+
+  if (rcrs$IsGeographic) {
+    msg <- glue::glue("The raster is in a geographic coordinate system but needs
+                       to be in a projected coordinate system supported by the database.")
+    stop(msg)
+  }
+
+  EPSG <- rcrs$epsg
+
+  # Check that this projection is supported by the database and,
+  # if so, get the datum and zone.
+  cmd <- glue::glue("select datum, zone
+                     from supported_crs
+                     where srid = {EPSG};")
+
+  res <- DBI::dbGetQuery(db, cmd)
+
+  if (nrow(res) != 1) {
+    msg <- glue::glue("The coordinate reference system (EPSG:{epsgcode})
+                       is not (yet) supported by the database.")
+    stop(msg)
+  }
+
+  ZONE <- res$zone
+  DATUM <- res$datum
+
+  # Table name
+  TBL_NAME <- glue::glue("vegheight_{tolower(DATUM)}_zone{ZONE}")
+
+
+  # Check that the table exists
+  if (!pg_table_exists(db, TBL_NAME)) {
+    msg <- glue::glue("No vegetation height table for {DATUM}, zone {ZONE}")
+    stop(msg)
+  }
+
+  # Check for meta-data record
+  #
+  if (!(is.null(tile_id) || is.na(tile_id))) {
+    # tile_id was provided: check that it exists in the metadata table
+    cmd <- glue::glue("select exists (
+                         select 1 from lidar.metadata
+                         where tile_id = {tile_id}
+                       );")
+
+    ok <-DBI::dbGetQuery(db, cmd)$exists
+
+    if (!ok) {
+      msg <- glue::glue("No meta-data record was found with tile_id = {tile_id}.")
+      stop(msg)
+    }
+
+  } else {
+    # tile_id was not provided: try to find it by matching the input
+    # raster file name
+    fname <- fs::path_file(vsi_url) %>%
+      fs::path_ext_remove(.)
+
+    cmd <- glue::glue("select tile_id from lidar.metadata
+                       where filename = '{fname}' and
+                       datum = '{DATUM}' and
+                       zone = {ZONE};")
+
+    res <-DBI::dbGetQuery(db, cmd)
+
+    if (nrow(res) == 0) {
+      msg <- glue::glue("No existing record was found that corresponds to this
+                         raster's file name and map projection in the meta-data table.")
+      stop(msg)
+
+    } else if (nrow(res) > 1) {
+      msg <- glue::glue("More than one meta-data record found that corresponds
+                         to this file name, datum and zone.
+
+                         This should never happen! Please check the database.")
+      stop(msg)
+    }
+
+    tile_id <- res$tile_id
+  }
+
+  # Check for an existing raster record
+  cmd <- glue::glue("select exists(
+                      select 1 from {TBL_NAME}
+                      where tile_id = {tile_id}
+                    )")
+
+  res <- DBI::dbGetQuery(db, cmd)
+  if (res$exists) {
+    msg <- glue::glue("There is an existing raster record in the raster table
+                       {TBL_NAME} with a matching tile_id value ({tile_id}).
+                       Please check before proceeding further.")
+    stop(msg)
+  }
+
+  x <- rlang::hash(Sys.time())
+  TEMP_LOAD_TABLE <- glue::glue("temp_load_{x}")
+
+  # Use the command line raster2pgsql program to create the SQL
+  # for the raster import
+  fsql <- tempfile(pattern = "sql", fileext = ".txt")
+
+  args <- glue::glue("-s {EPSG} -t {tilew}x{tilew} \\
+                     -I -R {vsi_url} \\
+                     lidar.{TEMP_LOAD_TABLE}")
+
+  system2(command = R2P, args = args, stdout = fsql)
+
+  # Run the generated SQL to load the raster to the temporary load table
+  queries <- readLines(fsql)
+  sapply(queries, function(q) DBI::dbExecute(db, q))
+
+  # Copy the raster from the temp_load table to the point
+  # counts table for this map zone
+
+  cmd <- glue::glue("
+    insert into lidar.{TBL_NAME} (tile_id, rast)
+    select {tile_id} as tile_id,
+    rast from lidar.{TEMP_LOAD_TABLE};")
+
+  res <- DBI::dbExecute(db, cmd)
+
+  # Drop the temp load table
+  cmd <- glue::glue("drop table lidar.{TEMP_LOAD_TABLE};")
+  DBI::dbExecute(db, cmd)
 
   if (!is.null(res) && res > 0) {
     TRUE
