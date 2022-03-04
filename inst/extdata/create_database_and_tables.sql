dee367c1fe25635c5e937b3085b03b6fd2928ff9
Michael Bedward <michael.bedward@gmail.com>
2022-03-04 13:00:34 +1100

    updated SQL for database creation

--- inst/extdata/create_database_and_tables.sql
+++ inst/extdata/create_database_and_tables.sql
@@ -42,7 +42,7 @@ BEGIN
 	END IF;
 END
 $do$;
-	  
+
 GRANT CONNECT ON DATABASE cermb_lidar TO readonly_user;
 
 GRANT USAGE ON SCHEMA lidar TO readonly_user;
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS lidar.stratum (
 	label text NOT NULL,
 	lowerht numeric(5, 2) NOT NULL,
 	upperht numeric(5, 2) NOT NULL CHECK (upperht > lowerht),
-	
+
 	PRIMARY KEY (strata_def, index)
 );
 
@@ -112,12 +112,12 @@ CREATE INDEX idx_stratum_strata_def
 CREATE INDEX idx_stratum_layer_index
 	ON lidar.stratum USING btree
 	(strata_def, index);
-	
+
 CREATE INDEX idx_stratum_layer_label
 	ON lidar.stratum USING btree
 	(strata_def, label);
-	
-	
+
+
 ----------------------------------------------------
 -- Table for 1:100,000 topo map key.
 -- Note: presently using GDA94 lon-lat coordinates.
@@ -137,7 +137,7 @@ GRANT ALL ON TABLE lidar.maps100k TO postgres;
 CREATE INDEX idx_maps100k_mapname
 	ON lidar.maps100k USING btree
 	(mapname);
-	
+
 
 ---------------------------------------------------------------
 -- LiDAR tile metadata table. Has all tile boundary polygons
@@ -155,14 +155,14 @@ CREATE TABLE IF NOT EXISTS lidar.metadata (
 	capture_year integer NOT NULL CHECK (capture_year >= 2000),
 	capture_start timestamp without time zone NOT NULL,
 	capture_end timestamp without time zone NOT NULL CHECK (capture_end > capture_start),
-	nflightlines integer NOT NULL CHECK (nflightlines >= 0), 
+	nflightlines integer NOT NULL CHECK (nflightlines >= 0),
 	npts_ground integer NOT NULL CHECK (npts_ground >= 0),
 	npts_veg integer NOT NULL CHECK (npts_veg >= 0),
 	npts_water integer NOT NULL CHECK (npts_water >= 0),
 	npts_other integer NOT NULL CHECK (npts_other >= 0),
 	point_density numeric NOT NULL CHECK (point_density > 0),
 	geom GEOMETRY(Polygon, 8058) NOT NULL  -- GDA 2020 / NSW Lambert projection
-);  
+);
 
 
 ALTER TABLE lidar.metadata
@@ -177,12 +177,12 @@ CREATE INDEX sidx_metadata
 CREATE INDEX idx_metadata_mapname
 	ON lidar.metadata USING btree
 	(mapname);
-	
+
 CREATE INDEX idx_metadata_year
 	ON lidar.metadata USING btree
 	(capture_year);
-	
-	
+
+
 ------------------------------------------------------------
 ------------------------------------------------------------
 -- Raster tables for point counts and veg cover
@@ -198,7 +198,7 @@ CREATE OR REPLACE FUNCTION lidar.check_datum_zone(
 			WHERE tile_id = $1 AND datum = $2 AND zone = $3)
 	$$
 	LANGUAGE SQL;
-	
+
 ALTER FUNCTION lidar.check_datum_zone
 	OWNER TO postgres;
 
@@ -213,7 +213,7 @@ GRANT ALL ON FUNCTION lidar.check_datum_zone TO postgres;
 -- bands; if 'is_veg' is true, the number of bands must be
 -- the number of strata_def bands minus one (for the ground
 -- layer).
-	
+
 CREATE OR REPLACE FUNCTION check_strata_def(strata_def text, rast raster, is_veg boolean) RETURNS boolean
 	AS $$
 		SELECT EXISTS (
@@ -227,7 +227,7 @@ ALTER FUNCTION lidar.check_strata_def
 
 GRANT ALL ON FUNCTION lidar.check_datum_zone TO postgres;
 
-	
+
 ------------------------------------------------------------
 -- Raster tables for MGA Zone 55 / GDA94 data (EPSG: 28355)
 
@@ -238,10 +238,10 @@ CREATE TABLE IF NOT EXISTS lidar.pointcounts_gda94_zone55 (
 	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
 	strata_def text NOT NULL REFERENCES lidar.strata_defs (abbrev),
 	rast raster,
-	
-	CONSTRAINT pointcounts_gda94_zone55_datum_zone 
+
+	CONSTRAINT pointcounts_gda94_zone55_datum_zone
 		CHECK (check_datum_zone(tile_id, datum => 'GDA94', zone => 55)),
-	
+
 	CONSTRAINT pointcounts_gda94_zone55_strata_def
 		CHECK (check_strata_def(strata_def, rast, is_veg => false))
 );
@@ -255,7 +255,7 @@ GRANT ALL ON TABLE lidar.pointcounts_gda94_zone55 TO postgres;
 CREATE INDEX IF NOT EXISTS sidx_pointcounts_gda94_zone55
     ON lidar.pointcounts_gda94_zone55 USING gist
     (st_convexhull(rast));
-	
+
 
 -- Vegetation cover rasters (allow differing strata defs)
 
@@ -264,10 +264,10 @@ CREATE TABLE IF NOT EXISTS lidar.vegcover_gda94_zone55 (
 	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
 	strata_def text NOT NULL REFERENCES lidar.strata_defs (abbrev),
 	rast raster,
-	
-	CONSTRAINT vegcover_gda94_zone55_datum_zone 
+
+	CONSTRAINT vegcover_gda94_zone55_datum_zone
 		CHECK (check_datum_zone(tile_id, datum => 'GDA94', zone => 55)),
-	
+
 	CONSTRAINT vegcover_gda94_zone55_strata_defs
 		CHECK (check_strata_defs(strata_def, rast, is_veg => true))
 );
@@ -283,6 +283,27 @@ CREATE INDEX IF NOT EXISTS sidx_vegcover_gda94_zone55
     (st_convexhull(rast));
 
 
+-- Vegetation height rasters
+
+CREATE TABLE IF NOT EXISTS lidar.vegheight_gda94_zone55 (
+	rid serial PRIMARY KEY,
+	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
+	rast raster,
+
+	CONSTRAINT vegheight_gda94_zone55_datum_zone
+		CHECK (check_datum_zone(tile_id, datum => 'GDA94', zone => 55))
+);
+
+ALTER TABLE IF EXISTS lidar.vegheight_gda94_zone55
+    OWNER to postgres;
+
+GRANT ALL ON TABLE lidar.vegheight_gda94_zone55 TO postgres;
+
+CREATE INDEX IF NOT EXISTS sidx_vegheight_gda94_zone55
+    ON lidar.vegheight_gda94_zone55 USING gist
+    (st_convexhull(rast));
+
+
 ------------------------------------------------------------
 -- Raster tables for MGA Zone 56 / GDA94 data (EPSG: 28356)
 
@@ -293,10 +314,10 @@ CREATE TABLE IF NOT EXISTS lidar.pointcounts_gda94_zone56 (
 	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
 	strata_def text NOT NULL REFERENCES lidar.strata_defs (abbrev),
 	rast raster,
-		
-	CONSTRAINT pointcounts_gda94_zone56_datum_zone 
+
+	CONSTRAINT pointcounts_gda94_zone56_datum_zone
 		CHECK (check_datum_zone(tile_id, datum => 'GDA94', zone => 56)),
-	
+
 	CONSTRAINT pointcounts_gda94_zone56_strata_def
 		CHECK (check_strata_def(strata_def, rast, is_veg => false))
 );
@@ -318,10 +339,10 @@ CREATE TABLE IF NOT EXISTS lidar.vegcover_gda94_zone56 (
 	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
 	strata_def text NOT NULL REFERENCES lidar.strata_defs (abbrev),
 	rast raster,
-			
-	CONSTRAINT vegcover_gda94_zone56_datum_zone 
+
+	CONSTRAINT vegcover_gda94_zone56_datum_zone
 		CHECK (check_datum_zone(tile_id, datum => 'GDA94', zone => 56)),
-	
+
 	CONSTRAINT vegcover_gda94_zone56_strata_defs
 		CHECK (check_strata_defs(strata_def, rast, is_veg => true))
 );
@@ -336,6 +357,28 @@ CREATE INDEX IF NOT EXISTS sidx_vegcover_gda94_zone56
     (st_convexhull(rast));
 
 
+-- Vegetation height rasters
+
+CREATE TABLE IF NOT EXISTS lidar.vegheight_gda94_zone56 (
+	rid serial PRIMARY KEY,
+	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
+	rast raster,
+
+	CONSTRAINT vegheight_gda94_zone56_datum_zone
+		CHECK (check_datum_zone(tile_id, datum => 'GDA94', zone => 56))
+);
+
+ALTER TABLE IF EXISTS lidar.vegheight_gda94_zone56
+    OWNER to postgres;
+
+GRANT ALL ON TABLE lidar.vegheight_gda94_zone56 TO postgres;
+
+CREATE INDEX IF NOT EXISTS sidx_vegheight_gda94_zone56
+    ON lidar.vegheight_gda94_zone56 USING gist
+    (st_convexhull(rast));
+
+
+
 -------------------------------------------------------------
 -- Raster tables for MGA Zone 55 / GDA2020 data (EPSG: 7855)
 
@@ -347,9 +390,9 @@ CREATE TABLE IF NOT EXISTS lidar.pointcounts_gda2020_zone55 (
 	strata_def text NOT NULL REFERENCES lidar.strata_defs (abbrev),
 	rast raster,
 
-	CONSTRAINT pointcounts_gda2020_zone55_datum_zone 
+	CONSTRAINT pointcounts_gda2020_zone55_datum_zone
 		CHECK (check_datum_zone(tile_id, datum => 'GDA2020', zone => 55)),
-	
+
 	CONSTRAINT pointcounts_gda2020_zone55_strata_def
 		CHECK (check_strata_def(strata_def, rast, is_veg => false))
 );
@@ -371,10 +414,10 @@ CREATE TABLE IF NOT EXISTS lidar.vegcover_gda2020_zone55 (
 	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
 	strata_def text NOT NULL REFERENCES lidar.strata_defs (abbrev),
 	rast raster,
-			
-	CONSTRAINT vegcover_gda2020_zone55_datum_zone 
+
+	CONSTRAINT vegcover_gda2020_zone55_datum_zone
 		CHECK (check_datum_zone(tile_id, datum => 'gda2020', zone => 55)),
-	
+
 	CONSTRAINT vegcover_gda2020_zone55_strata_def
 		CHECK (check_strata_def(strata_def, rast, is_veg => true))
 );
@@ -389,6 +432,27 @@ CREATE INDEX IF NOT EXISTS sidx_vegcover_gda2020_zone55
     (st_convexhull(rast));
 
 
+-- Vegetation height rasters
+
+CREATE TABLE IF NOT EXISTS lidar.vegheight_gda2020_zone55 (
+	rid serial PRIMARY KEY,
+	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
+	rast raster,
+
+	CONSTRAINT vegheight_gda2020_zone55_datum_zone
+		CHECK (check_datum_zone(tile_id, datum => 'GDA2020', zone => 55))
+);
+
+ALTER TABLE IF EXISTS lidar.vegheight_gda2020_zone55
+    OWNER to postgres;
+
+GRANT ALL ON TABLE lidar.vegheight_gda2020_zone55 TO postgres;
+
+CREATE INDEX IF NOT EXISTS sidx_vegheight_gda2020_zone55
+    ON lidar.vegheight_gda2020_zone55 USING gist
+    (st_convexhull(rast));
+
+
 -----------------------------------------------------
 -- Tables for MGA Zone 56 / GDA2020 data (EPSG: 7856)
 
@@ -400,9 +464,9 @@ CREATE TABLE IF NOT EXISTS lidar.pointcounts_gda2020_zone56 (
 	strata_def text NOT NULL REFERENCES lidar.strata_defs (abbrev),
 	rast raster,
 
-	CONSTRAINT pointcounts_gda2020_zone56_datum_zone 
+	CONSTRAINT pointcounts_gda2020_zone56_datum_zone
 		CHECK (check_datum_zone(tile_id, datum => 'GDA2020', zone => 56)),
-	
+
 	CONSTRAINT pointcounts_gda2020_zone56_strata_def
 		CHECK (check_strata_def(strata_def, rast, is_veg => false))
 );
@@ -424,10 +488,10 @@ CREATE TABLE IF NOT EXISTS lidar.vegcover_gda2020_zone56 (
 	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
 	strata_def text NOT NULL REFERENCES lidar.strata_defs (abbrev),
 	rast raster,
-			
-	CONSTRAINT vegcover_gda2020_zone56_datum_zone 
+
+	CONSTRAINT vegcover_gda2020_zone56_datum_zone
 		CHECK (check_datum_zone(tile_id, datum => 'GDA2020', zone => 56)),
-	
+
 	CONSTRAINT vegcover_gda2020_zone56_strata_def
 		CHECK (check_strata_def(strata_def, rast, is_veg => true))
 );
@@ -442,6 +506,27 @@ CREATE INDEX IF NOT EXISTS sidx_vegcover_gda2020_zone56
     (st_convexhull(rast));
 
 
+-- Vegetation height rasters
+
+CREATE TABLE IF NOT EXISTS lidar.vegheight_gda2020_zone56 (
+	rid serial PRIMARY KEY,
+	tile_id integer NOT NULL REFERENCES lidar.metadata (tile_id),
+	rast raster,
+
+	CONSTRAINT vegheight_gda2020_zone56_datum_zone
+		CHECK (check_datum_zone(tile_id, datum => 'GDA2020', zone => 56))
+);
+
+ALTER TABLE IF EXISTS lidar.vegheight_gda2020_zone56
+    OWNER to postgres;
+
+GRANT ALL ON TABLE lidar.vegheight_gda2020_zone56 TO postgres;
+
+CREATE INDEX IF NOT EXISTS sidx_vegheight_gda2020_zone56
+    ON lidar.vegheight_gda2020_zone56 USING gist
+    (st_convexhull(rast));
+
+
 -- Summary of coordinate reference systems supported by the database.
 --   TODO: convert this to a dynamic query rather than using hard-coded
 --   data.
@@ -449,8 +534,8 @@ DROP TABLE IF EXISTS lidar.supported_crs;
 
 CREATE TABLE lidar.supported_crs AS
 SELECT * FROM (
-	VALUES 
-	('GDA94', 55, 28355), 
+	VALUES
+	('GDA94', 55, 28355),
 	('GDA94', 56, 28356),
 	('GDA2020', 55, 7855),
 	('GDA2020', 56, 7856)
@@ -458,7 +543,7 @@ SELECT * FROM (
 
 ALTER TABLE lidar.supported_crs
     OWNER TO postgres;
-	
+
 GRANT ALL ON TABLE lidar.supported_crs TO postgres;
 
 
@@ -479,7 +564,7 @@ CREATE OR REPLACE VIEW lidar.coverage AS
 
 ALTER TABLE lidar.coverage
     OWNER TO postgres;
-	
+
 GRANT ALL ON TABLE lidar.coverage TO postgres;
 
 
@@ -494,25 +579,25 @@ GRANT ALL ON TABLE lidar.coverage TO postgres;
 -- This is useful to identify clusters of lidar tiles that are close
 -- spatially and temporally.
 --
--- Adapted from: 
+-- Adapted from:
 -- http://www.lexykassan.com/coding-conundrums/calculating-months-between-dates-in-postgresql/
 
 CREATE OR REPLACE FUNCTION lidar.month_diff(
-	start_datetime timestamp without time zone, 
+	start_datetime timestamp without time zone,
 	end_datetime timestamp without time zone,
     abs_diff boolean = true)
 	returns numeric
 	as $$
 		WITH t AS (
 			SELECT (
-				EXTRACT(year FROM AGE(end_datetime, start_datetime))*12 + 
+				EXTRACT(year FROM AGE(end_datetime, start_datetime))*12 +
 				EXTRACT(month FROM AGE(end_datetime, start_datetime))
 			)::numeric AS months
 		)
 		SELECT CASE
 			WHEN abs_diff THEN ABS(months)
 			ELSE months
-		END 
+		END
 		FROM t;
 	$$
 	language sql;
@@ -531,7 +616,7 @@ CREATE OR REPLACE FUNCTION lidar.parse_filename(filename text)
 	RETURNS table(name text, year integer, month integer, east_km integer, north_km integer, zone integer) AS
 $$
 	WITH parts AS (
-		SELECT 
+		SELECT
 			regexp_match(filename,
 						 '^([A-Za-z]+)(\d{4})(\d{2}).+AHD_(\d{3})(\d{4})_(\d{2})') AS p
 	)
@@ -543,7 +628,7 @@ $$
 		(p[5])::integer AS north_km,
 		(p[6])::integer AS zone
 	FROM parts;
-$$ 
+$$
 LANGUAGE sql;
 
 
@@ -564,7 +649,7 @@ BEGIN
 		WHERE tt.table_schema = 'lidar' AND tt.table_name ~* '^(pointcounts|vegcover)'
 		ORDER BY tt.table_name
 	LOOP
-		RETURN QUERY EXECUTE format('select cast(%L as text), count(*) 
+		RETURN QUERY EXECUTE format('select cast(%L as text), count(*)
 									from lidar.%I',
 									tblname, tblname);
 	END LOOP;
@@ -572,7 +657,7 @@ END
 $$ LANGUAGE plpgsql;
 
 
--- Function to get table name and tile_id from the combined 
+-- Function to get table name and tile_id from the combined
 -- pointcounts* raster tables.
 --
 -- Example usage:
@@ -589,7 +674,7 @@ BEGIN
 		WHERE tt.table_schema = 'lidar' AND tt.table_name ~* '^pointcounts'
 		ORDER BY tt.table_name
 	LOOP
-		RETURN QUERY EXECUTE format('select cast(%L as text), tile_id 
+		RETURN QUERY EXECUTE format('select cast(%L as text), tile_id
 									from lidar.%I',
 									tblname, tblname);
 	END LOOP;
@@ -613,10 +698,54 @@ BEGIN
 		WHERE tt.table_schema = 'lidar' AND tt.table_name ~* '^vegcover'
 		ORDER BY tt.table_name
 	LOOP
-		RETURN QUERY EXECUTE format('select cast(%L as text), tile_id 
+		RETURN QUERY EXECUTE format('select cast(%L as text), tile_id
 									from lidar.%I',
 									tblname, tblname);
 	END LOOP;
+END
+$$ LANGUAGE plpgsql;
+
+
+-- Function to get the S3 storage path to a raster file of
+-- point counts or Specht veg cover corresponding to a given tile_id.
+-- Example usage:
+-- select get_s3path(33822, 'p')  -- for point counts raster
+-- select get_s3path(33822, 'v')  -- for Specht veg cover raster
+--
+CREATE OR REPLACE FUNCTION lidar.get_raster_s3path(tid int, filetype text) RETURNS text
+AS $$
+DECLARE
+	tblprefix text;
+	tblname text;
+	s3path text;
+BEGIN
+  IF LOWER(filetype) = 'p' THEN
+		tblprefix := 'pointcounts_';
+	ELSIF LOWER(filetype) = 'v' THEN
+		tblprefix := 'vegcover_';
+	ELSIF LOWER(filetype) = 'h' THEN
+		tblprefix := 'vegheight_';
+	ELSE
+		RAISE EXCEPTION 'The filetype argument must be one of p (point counts), v (veg cover) or h (veg height)';
+	END IF;
+
+	SELECT tblprefix || LOWER(datum) || '_zone' || zone::text
+	INTO tblname
+	FROM lidar.metadata
+	WHERE tile_id = tid;
+
+	IF NOT found THEN
+		return NULL;
+	ELSE
+		EXECUTE format('SELECT (st_bandmetadata(rast)).path
+						FROM %I
+						WHERE tile_id = $1;',
+						tblname)
+		INTO s3path
+		USING tid;
+
+    	RETURN s3path;
+	END IF;
 END
 $$ LANGUAGE plpgsql;
 
